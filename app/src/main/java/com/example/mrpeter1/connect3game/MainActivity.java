70093761c839384e6c62b7cebc5347536add9b8b
muh ikhsan <thisisli96@yahoo.com>
2019-12-05 10:53:55 +0700

    step step kedua dan ketiga mengkosongkan dulu gambar gambarnya . setelah itu set gambar muncul sesuai tempat yang di klik . dan gambarnya jga sudah sesuai player yang klik atau swicth

--- app/src/main/java/com/example/mrpeter1/connect3game/MainActivity.java
+++ app/src/main/java/com/example/mrpeter1/connect3game/MainActivity.java
@@ -3,9 +3,29 @@ package com.example.mrpeter1.connect3game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    // 0 = yellow , 1 = red
+    int activePlayer = 0;
+    public  void dropIn(View view){
 
+
+        ImageView counter = (ImageView) view;
+        counter.setTranslationY(-1500);
+        if (activePlayer == 0 ){
+
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
+        } else {
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
+
+        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
